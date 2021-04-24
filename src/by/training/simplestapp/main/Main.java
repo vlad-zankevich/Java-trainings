1c7898aa4a8ab63f0865b97f1d124309f77a8028
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-04-24 17:33:15 +0300

    Update Main.java

--- src/by/training/simplestapp/main/Main.java
+++ src/by/training/simplestapp/main/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-      int x=1;
-      System.out.println(x);
+      int x = 1;
+      int y = 2;
+
+      System.out.println("x=" + x);
+      System.out.println("y=" + y);
     }
 }
