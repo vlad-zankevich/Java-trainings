d2f29a23944f2b312f12cb01a8024f15a2ab61d5
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-04-24 18:18:06 +0300

    spaces added

--- src/by/training/simplestapp/main/Main.java
+++ src/by/training/simplestapp/main/Main.java
@@ -3,9 +3,9 @@ package by.training.simplestapp.main;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    // write your code here
 
-      System.out.println("Hello World");
+        System.out.println("Hello World");
 
     }
 }
--- src/by/training/simplestapp/main/PersonalInfo.java
+++ src/by/training/simplestapp/main/PersonalInfo.java
@@ -1,13 +1,13 @@
 package by.training.simplestapp.main;
 
 public class PersonalInfo {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String a = "Занкевич Владислав Николаевич";
-    String b = "г. Гомель, ул. Революционная, 12";
-    String c = "+37529 137 88 32";
+        String a = "Занкевич Владислав Николаевич";
+        String b = "г. Гомель, ул. Революционная, 12";
+        String c = "+37529 137 88 32";
 
-    System.out.print(a + ", " + b + ", " + c);
+        System.out.print(a + ", " + b + ", " + c);
 
   }
 
